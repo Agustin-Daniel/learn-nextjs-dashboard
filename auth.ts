a0fd6a5708148383fefb67cfb755ff6c57e4290b
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-11-19 19:58:28 -0300

    chapter 16

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import { sql } from '@vercel/postgres';
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
@@ -20,20 +21,22 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        // ...
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
  
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
- 
-          if (passwordsMatch) return user;
-        }
- 
-        console.log('Invalid credentials');
-        return null;
-      },
-    }),
-  ],
-});
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email);
+            if (!user) return null;
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+   
+            if (passwordsMatch) return user;
+          }
+   
+          console.log('Invalid credentials');
+          return null;
+        },
+      }),
+    ],
+  });
